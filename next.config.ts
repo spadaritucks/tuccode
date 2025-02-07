13f4a8de5bcd4476539bddea0f88fb2e52d55479
spadaritucks <thiago.spadari02@gmail.com>
2025-02-07 14:48:37 -0300

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/api/sitemap",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
